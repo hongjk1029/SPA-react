8e2ebdc220dba377f341103b053ed78bb796362e
Daniel Lim Zheng Yu <daniellim0510@Daniel-Lim-MacAir.local>
2022-10-10 15:11:44 +0800

    http request

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -53,10 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:3000',
-    'http://localhost:3000'
-]
+
 
 ROOT_URLCONF = 'core.urls'
 
@@ -130,3 +127,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ORIGIN_WHITELIST = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 from user.routers import router
     
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/', include((router.urls, 'restaurant'), namespace='restaurant'))
 ]
