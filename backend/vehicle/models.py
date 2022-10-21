1fb8c9d13e5a00acc79ad90c6205a8387dd9e839
Daniel Lim Zheng Yu <daniellim0510@Daniel-Lim-MacAir.local>
2022-10-21 20:56:04 +0800

    vehicle database design

---
+++ backend/vehicle/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+
+# Create your models here.
+class VehicleBrand(models.Model):
+    brand_name = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+sales_type = [
+    ('Sales','Sales'),
+    ('Rent','Rent'),
+]
+
+fuel_type = [
+    ('Petrol','Petrol'),
+    ('Hybrid','Hybrid'),
+    ('EV','EV'),
+]
+
+class VehicleImage(models.Model):   
+    vehical_image = models.ImageField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class VehicleDetails(models.Model):
+    fuel_type = models.CharField(max_length=255)
+    model_year = models.IntegerField()
+    vehicle_milleage = models.IntegerField()
+    seating_capacity = models.IntegerField()
+    air_conditioner = models.BooleanField()
+    power_door_locks = models.BooleanField()
+    anti_lock_brake = models.BooleanField()
+    brake_assist = models.BooleanField()
+    power_steering = models.BooleanField()
+    driver_airbag = models.BooleanField()
+    passenger_airbag = models.BooleanField()
+    power_window = models.BooleanField()
+    cd_player = models.BooleanField()
+    central_locking = models.BooleanField()
+    crash_sensor = models.BooleanField()
+    leather_seat = models.BooleanField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class VehicleRent(models.Model):
+    rent_active = models.BooleanField(default=False)
+    price_per_week = models.DecimalField(default=0, max_digits=999,decimal_places=2)
+    price_per_month = models.DecimalField(default=0, max_digits=999,decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class VehicleDocument(models.Model):
+    document = models.FileField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Vehicle(models.Model):
+    vehical_name = models.CharField(max_length=255)
+    vehical_brand = models.ForeignKey(VehicleBrand, on_delete=models.CASCADE)
+    vehical_overview = models.CharField(max_length=255)  
+    number_plate = models.CharField(max_length=10)
+    is_active = models.BooleanField()
+    vehical_image = models.ForeignKey(VehicleImage, on_delete=models.CASCADE)
+    vehicle_rent_active = models.OneToOneField(VehicleRent,on_delete=models.CASCADE)
+    price_of_cost = models.DecimalField(max_digits=999,decimal_places=2)
+    price_of_sale = models.DecimalField(max_digits=999,decimal_places=2)
+    vehicle_details = models.OneToOneField(VehicleDetails, on_delete=models.CASCADE)
+    vehicle_document = models.ForeignKey(VehicleDocument, on_delete=models.CASCADE)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
