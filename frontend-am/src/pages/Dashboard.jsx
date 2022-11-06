0fd35a9e32facc96479821d3a31394f3c308ad6f
zheminlow <69181590+zheminlow@users.noreply.github.com>
2022-11-07 02:45:54 +0800

    Dashboard

--- frontend-am/src/pages/Dashboard.jsx
+++ frontend-am/src/pages/Dashboard.jsx
@@ -1,10 +1,37 @@
 import React from "react";
-import Helmet from "../components/Helmet/Helmet";
 import { Container, Row, Col } from "reactstrap";
+import Helmet from "../components/Helmet/Helmet";
+
+import "../styles/dashboard.css";
+import { FiArrowRight } from "react-icons/fi";
+import dashboardData from "../assets/data/dashboardData";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <Helmet title="Home">
+    <section>
+    <Container>
+      <Row>
+        <h2>Dashboard</h2>
+        <hr class="style1 text-secondary"></hr>
+        {dashboardData.slice(0, 5).map((item) => (
+          <Col lg="3" md="4" sm="6" className="mb-5">
+            <div class="card text-white mb-3 dashboard-card border-0">
+              <div className={item.background}>
+                <h1 class="text-center text-white">{item.amount}</h1>
+                <p class="text-center">{item.displayName}</p>
+              </div>
+              <div class="card-body text-center bg-light">
+                <a href={item.redirectUrl} class="card-link text-secondary text-decoration-none">FULL DETAIL <FiArrowRight/></a>
+              </div>
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  </section>
+  </Helmet>
+
   );
 };
 
