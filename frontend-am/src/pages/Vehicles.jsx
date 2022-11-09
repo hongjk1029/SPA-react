19176f35f8f10536208288b97e4726ef89416a4b
zheminlow <69181590+zheminlow@users.noreply.github.com>
2022-11-10 02:14:00 +0800

    Updated Vehicles and Contact Query

--- frontend-am/src/pages/Vehicles.jsx
+++ frontend-am/src/pages/Vehicles.jsx
@@ -1,11 +1,120 @@
 import React from "react";
-import Helmet from "../components/Helmet/Helmet";
 import { Container, Row, Col } from "reactstrap";
+import { FiEdit } from "react-icons/fi";
+import { IoMdClose } from "react-icons/io";
+import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import vehicleData from "../assets/data/vehicleData";
+import "../styles/common-section.css";
+
+const columns = [
+  {
+    dataField: 'id',
+    text: '#',
+    sort: true,
+    headerClasses: 'pointer'
+  }, 
+  {
+    dataField: 'name',
+    text: 'Vehicle Title',
+    sort: true,
+    headerClasses: 'pointer'
+  }, 
+  {
+    dataField: 'brand',
+    text: 'Brand',
+    sort: true,
+    headerClasses: 'pointer'
+  },
+  {
+    dataField: 'saleType',
+    text: 'Sale Type',
+    sort: true,
+    headerClasses: 'pointer'
+  },
+  {
+    dataField: 'rentalPrice',
+    text: 'Rental Price(RM)',
+    sort: true,
+    headerClasses: 'pointer'
+  },
+  {
+    dataField: 'salePrice',
+    text: 'Sale Price(RM)',
+    sort: true,
+    headerClasses: 'pointer'
+  },
+  {
+    dataField: 'fuelType',
+    text: 'Fuel Type',
+    sort: true,
+    headerClasses: 'pointer'
+  },
+  {
+    dataField: 'modelYear',
+    text: 'Model Year',
+    sort: true,
+    headerClasses: 'pointer'
+  },
+  {
+    dataField: 'active',
+    text: 'Status',
+    sort: true,
+    headerClasses: 'pointer',
+    formatter: (cellContent, row) => {
+      if (row.active) {
+        return (
+          <span class="badge bg-success">Active</span>
+        );
+      }
+      return (
+        <span class="badge bg-danger">Inactive</span>
+      );
+    }
+  },
+  {
+    dataField: 'action',
+    text: 'Action',
+    formatter: (cellContent, row) => {
+      return (
+        <div>
+          <FiEdit class="text-primary btnEdit" role="button"/>
+          <IoMdClose class="text-danger" role="button"/>
+        </div>
+      );
+    }
+  }
+];
+
+const defaultSorted = [{
+  dataField: 'id',
+  order: 'asc'
+}];
 
 const Vehicles = () => {
-  return (
-    <div>Vehicles</div>
-  );
+  return(
+    <section>
+      <Container>
+        <Row>
+          <h2>Manage Vehicles</h2>
+          <hr class="style1 text-secondary"></hr>
+          <BootstrapTable 
+            bootstrap4
+            keyField='id' 
+            data={ vehicleData } 
+            columns={ columns } 
+            defaultSorted={ defaultSorted } 
+            pagination={ paginationFactory() } 
+            striped
+            hover
+            condensed
+            noDataIndication={ 'no results found' }
+            headerClasses="noselect"
+          />
+        </Row>
+      </Container>
+    </section>
+  )
 };
 
 export default Vehicles;
