d790a253ccff10800a2fea72567cec3cc13e4139
zheminlow <69181590+zheminlow@users.noreply.github.com>
2022-11-28 23:03:13 +0800

    Add Missing Data File

---
+++ frontend/src/assets/data/dashboardData.js
@@ -0,0 +1,25 @@
+const dashboardData = [
+  {
+    id: 1001,
+    amount: 7,
+    displayName: "LISTED VEHICLES",
+    redirectUrl:"/admin/vehicles",
+    background: "card-header pt-4 bg-danger"
+  },
+  {
+    id: 1002,
+    amount: 13,
+    displayName: "LISTED BRANDS",
+    redirectUrl: "/admin/brands",
+    background: "card-header pt-4 bg-success"
+  },
+  {
+    id: 1003,
+    amount: 1,
+    displayName: "QUERIES",
+    redirectUrl: "/admin/manage-query",
+    background: "card-header pt-4 bg-primary"
+  },
+];
+
+export default dashboardData;
---
+++ frontend/src/assets/data/pageData.js
@@ -0,0 +1,39 @@
+const pageData = [
+  {
+    id: 0,
+    name: "-- Select --",
+    createdDate: "",
+    modifiedDate: "",
+    details: "",
+  },
+  {
+    id: 1,
+    name: "Terms and Condition",
+    createdDate: "2022-11-07 22:48:57",
+    modifiedDate: "2022-11-07 22:48:57",
+    details: "Terms and ConditionTerms and Condition",
+  },
+  {
+    id: 2,
+    name: "Privacy and Policy",
+    createdDate: "2022-11-07 22:48:57",
+    modifiedDate: "2022-11-07 22:48:57",
+    details: "Privacy and PolicyPrivacy and PolicyPrivacy and Policy",
+  },
+  {
+    id: 3,
+    name: "About Us",
+    createdDate: "2022-11-07 22:48:57",
+    modifiedDate: "2022-11-07 22:48:57",
+    details: "About UsAbout UsAbout Us",
+  },
+  {
+    id: 4,
+    name: "FAQs",
+    createdDate: "2022-11-07 22:48:57",
+    modifiedDate: "2022-11-07 22:48:57",
+    details: "FAQsFAQsFAQs",
+  },
+];
+
+export default pageData;
