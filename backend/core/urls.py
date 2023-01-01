0df82f3f61790a6b1f2160370637514afbbd6fe4
Daniel Lim Zheng Yu <daniellim0510@Daniel-Lim-MacAir.local>
2023-01-01 17:51:57 +0800

    fix bugs

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -24,12 +24,12 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="SPA Project API",
       default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      description="Spa Project V1 API Endpoint",
+    #   terms_of_service="https://www.google.com/policies/terms/",
+    #   contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="Jigo License"),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
