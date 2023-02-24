0c67ddf7ce98ccf1e1a9d6d346b0fcae2c8c39ce
Jasmine Chong <ccy030300@gmail.com>
2023-02-24 09:18:21 +0800

    [ADD] update vehicle + doc

--- backend/vehicle/serializers.py
+++ backend/vehicle/serializers.py
@@ -21,10 +21,11 @@ class VehicleImageSerializer(serializers.ModelSerializer):
 
 class VehicleSerializer(serializers.ModelSerializer):
     vehicle_documents = serializers.ListField(write_only=True, required=False)
+    delete_documents = serializers.ListField(write_only=True, required=False)
 
     class Meta:
         model = Vehicle
-        fields = ['id', 'vehicle', 'vehicle_brand', 'vehicle_overview', 'number_plate', 'price_of_cost', 'price_of_sale', 'vehicle_details', 'vehicle_documents',]
+        fields = ['id', 'vehicle', 'vehicle_brand', 'vehicle_overview', 'number_plate', 'price_of_cost', 'price_of_sale', 'vehicle_details', 'vehicle_documents', 'delete_documents']
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
@@ -40,3 +41,16 @@ class VehicleSerializer(serializers.ModelSerializer):
             create_vehicle_doc = create_vehicle.documents.bulk_create([VehicleDocument(document=doc, vehicle=create_vehicle) for doc in car_doc])
 
         return create_vehicle
+
+    def update(self, instance, validated_data):
+        delete_doc = validated_data.pop('delete_documents', [])
+        car_doc = validated_data.pop('vehicle_documents', [])
+        instance = super().update(instance, validated_data)
+        
+        if car_doc:
+            update_vehicle_doc = instance.documents.bulk_create([VehicleDocument(document=doc, vehicle=instance) for doc in car_doc])
+
+        if delete_doc:
+            instance.documents.filter(id__in=delete_doc).delete()
+            
+        return instance
