7ba5bb09674e3d2bc219dd79974b7f0c8e5bb932
Jasmine Chong <ccy030300@gmail.com>
2023-03-13 09:53:30 +0800

    [CHANGE] change update images format

--- backend/vehicle/serializers.py
+++ backend/vehicle/serializers.py
@@ -13,20 +13,23 @@ class VehicleBrandSerializer(serializers.ModelSerializer):
 class VehicleDocumentSerializer(serializers.ModelSerializer):
     class Meta:
         model = VehicleDocument
-        fields = ['document']
+        fields = ['id','document']
 
 class VehicleImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = VehicleImage
-        fields = ['vehicle_image']
+        fields = ['id','vehicle_image']
 
 class VehicleSerializer(serializers.ModelSerializer):
     accessories = serializers.ListField(write_only=True)
     vehicle_documents = serializers.ListField(write_only=True, required=False)
     vehicle_images = serializers.ListField(write_only=True)
+    delete_documents = serializers.ListField(write_only=True, required=False)
+    delete_images = serializers.ListField(write_only=True, required=False)
+    
     class Meta:
         model = Vehicle
-        fields = ['id', 'vehicle', 'vehicle_brand', 'vehicle_overview', 'number_plate', 'price_of_cost', 'price_of_sale', 'fuel_type', 'model_year', 'seating_capacity', 'mileage', 'accessories', 'vehicle_documents', 'vehicle_images']
+        fields = ['id', 'vehicle', 'vehicle_brand', 'vehicle_overview', 'number_plate', 'price_of_cost', 'price_of_sale', 'fuel_type', 'model_year', 'seating_capacity', 'mileage', 'accessories', 'vehicle_documents', 'vehicle_images', 'delete_documents', 'delete_images']
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
@@ -51,14 +54,18 @@ class VehicleSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         car_doc = validated_data.pop('vehicle_documents', [])
         car_images = validated_data.pop('vehicle_images', [])
+        delete_doc = validated_data.pop('delete_documents', [])
+        delete_images = validated_data.pop('delete_images', [])
         instance = super().update(instance, validated_data)
-
-        instance.documents.filter(vehicle_id=instance).delete()
-        instance.vehicle_image.filter(vehicle_id=instance).delete()
         
         if car_doc:
             update_vehicle_doc = instance.documents.bulk_create([VehicleDocument(document=doc, vehicle=instance) for doc in car_doc])
         if car_images:
             update_vehicle_image = instance.vehicle_image.bulk_create(VehicleImage(vehicle_image=image, vehicle=instance) for image in car_images)
+            
+        if delete_doc:
+            instance.documents.filter(id__in=delete_doc).delete()
+        if delete_images:
+            instance.vehicle_image.filter(id__in=delete_images).delete()
             
         return instance
