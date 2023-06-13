d2140cedea2bc44b6cd72d70bb28a39af99384a0
Daniel-Pacer <daniel-pacer@Daniel-Pacers-MacBook-Pro.local>
2023-06-13 09:39:50 +0800

    implement a lower cpu and ram

--- infra/backend/develop/main.tf
+++ infra/backend/develop/main.tf
@@ -4,7 +4,7 @@ module "infra_system" {
   db_instance    = "db.t4g.micro"
   db_storage     = "20"
   region         = "ap-southeast-1"
-  server_cpu     = 512
-  server_ram     = 1024
+  server_cpu     = 256
+  server_ram     = 512
   rds_multi_az   = "false"
 }
